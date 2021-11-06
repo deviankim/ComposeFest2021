d445430c6dc5e06596105800059638c68baed176
ssk <developer.kss@gmail.com>
2021-11-07 00:21:00 +0900

    rememberSaveable

--- exercise1/app/src/main/java/com/example/ex1/MainActivity.kt
+++ exercise1/app/src/main/java/com/example/ex1/MainActivity.kt
@@ -6,6 +6,7 @@ import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.*
 import androidx.compose.material.*
 import androidx.compose.runtime.*
+import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
@@ -25,7 +26,7 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 private fun MyApp() {
-    var shouldShowOnboarding by remember { mutableStateOf(true) }
+    var shouldShowOnboarding by rememberSaveable { mutableStateOf(true) }
 
     if (shouldShowOnboarding) {
         OnboardingScreen { shouldShowOnboarding = false }
