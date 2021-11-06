cff878d1063574e80b3afdef458dfa4b84e21e72
ssk <developer.kss@gmail.com>
2021-11-07 00:00:06 +0900

    extraPadding

--- exercise1/app/src/main/java/com/example/ex1/MainActivity.kt
+++ exercise1/app/src/main/java/com/example/ex1/MainActivity.kt
@@ -40,12 +40,15 @@ private fun MyApp(names: List<String> = listOf("World", "Compose")) {
 @Composable
 fun Greeting(name: String) {
     val expanded = remember { mutableStateOf(false) }
+    val extraPadding = if (expanded.value) 48.dp else 0.dp
     Surface(
         color = MaterialTheme.colors.primary,
         modifier = Modifier.padding(vertical = 4.dp, horizontal = 8.dp)
     ) {
         Row(modifier = Modifier.padding(24.dp)) {
-            Column(modifier = Modifier.weight(1f)) {
+            Column(modifier = Modifier
+                .weight(1f)
+                .padding(extraPadding)) {
                 Text("Hello, ")
                 Text(name)
             }
