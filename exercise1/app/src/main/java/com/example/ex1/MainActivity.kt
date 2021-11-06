a0949d00328f75f9203a2e5d0c724ece0d067cca
ssk <developer.kss@gmail.com>
2021-11-06 23:53:03 +0900

    introduce Row and OutlinedButton

--- exercise1/app/src/main/java/com/example/ex1/MainActivity.kt
+++ exercise1/app/src/main/java/com/example/ex1/MainActivity.kt
@@ -4,9 +4,10 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material.MaterialTheme
+import androidx.compose.material.OutlinedButton
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
@@ -40,9 +41,14 @@ fun Greeting(name: String) {
         color = MaterialTheme.colors.primary,
         modifier = Modifier.padding(vertical = 4.dp, horizontal = 8.dp)
     ) {
-        Column(modifier = Modifier.padding(24.dp).fillMaxWidth()) {
-            Text("Hello, ")
-            Text(name)
+        Row(modifier = Modifier.padding(24.dp)) {
+            Column(modifier = Modifier.weight(1f)) {
+                Text("Hello, ")
+                Text(name)
+            }
+            OutlinedButton(onClick = { /*TODO*/ }) {
+                Text("Show more")
+            }
         }
     }
 }
