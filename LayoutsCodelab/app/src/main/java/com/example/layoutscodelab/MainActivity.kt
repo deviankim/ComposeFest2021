5221abb48e9acbcdcd89248eed670dcfe45302ca
ssk <developer.kss@gmail.com>
2021-11-07 17:17:09 +0900

    Scrolling List

--- LayoutsCodelab/app/src/main/java/com/example/layoutscodelab/MainActivity.kt
+++ LayoutsCodelab/app/src/main/java/com/example/layoutscodelab/MainActivity.kt
@@ -14,6 +14,7 @@ import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Favorite
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.CompositionLocalProvider
+import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
@@ -22,6 +23,7 @@ import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import coil.compose.rememberImagePainter
 import com.example.layoutscodelab.ui.theme.LayoutsCodelabTheme
+import kotlinx.coroutines.launch
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -54,7 +56,8 @@ fun LayoutsCodelab() {
         BodyContent(
             Modifier
                 .padding(innerPadding)
-                .padding(8.dp))
+                .padding(8.dp)
+        )
     }
 }
 
@@ -110,13 +113,38 @@ fun SimpleList() {
 }
 
 @Composable
-fun ImageList() {
+fun ScrollingList() {
+    val listSize = 100
     // We save the scrolling position with this state
     val scrollState = rememberLazyListState()
+    // We save the coroutine scope where our animated scroll will be executed
+    val coroutineScope = rememberCoroutineScope()
 
-    LazyColumn(state = scrollState) {
-        items(100) { index ->
-            ImageListItem(index)
+    Column {
+        Row {
+            Button(onClick = {
+                coroutineScope.launch {
+                    // 0 is the first item index
+                    scrollState.animateScrollToItem(0)
+                }
+            }) {
+                Text("Scroll to the top")
+            }
+
+            Button(onClick = {
+                coroutineScope.launch {
+                    // listSize - 1 is the last index of the list
+                    scrollState.animateScrollToItem(listSize - 1)
+                }
+            }) {
+                Text("Scroll to the end")
+            }
+        }
+
+        LazyColumn(state = scrollState) {
+            items(listSize) {
+                ImageListItem(it)
+            }
         }
     }
 }
@@ -141,7 +169,7 @@ fun ImageListItem(index: Int) {
 @Composable
 fun SimpleListPreview() {
     LayoutsCodelabTheme {
-        ImageList()
+        ScrollingList()
     }
 }
 
