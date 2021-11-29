dd08d1c724032c3adb23a689ddd6e71089d4bc12
ssk <developer.kss@gmail.com>
2021-11-29 18:37:51 +0900

    State/start

--- codelab/UsingState/app/src/main/java/com/codelabs/state/todo/Data.kt
+++ codelab/UsingState/app/src/main/java/com/codelabs/state/todo/Data.kt
@@ -18,10 +18,14 @@ package com.codelabs.state.todo
 
 import androidx.annotation.StringRes
 import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.*
+import androidx.compose.material.icons.filled.CropSquare
+import androidx.compose.material.icons.filled.Done
+import androidx.compose.material.icons.filled.Event
+import androidx.compose.material.icons.filled.PrivacyTip
+import androidx.compose.material.icons.filled.RestoreFromTrash
 import androidx.compose.ui.graphics.vector.ImageVector
 import com.codelabs.state.R
-import java.util.*
+import java.util.UUID
 
 data class TodoItem(
     val task: String,
@@ -31,7 +35,7 @@ data class TodoItem(
 )
 
 enum class TodoIcon(val imageVector: ImageVector, @StringRes val contentDescription: Int) {
-    Square(Icons.Default.CropSquare, R.string.cd_crop_square),
+    Square(Icons.Default.CropSquare, R.string.cd_expand),
     Done(Icons.Default.Done, R.string.cd_done),
     Event(Icons.Default.Event, R.string.cd_event),
     Privacy(Icons.Default.PrivacyTip, R.string.cd_privacy),
